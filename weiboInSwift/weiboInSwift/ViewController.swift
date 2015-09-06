2977c7e3899a65110c4f260edbbd698b8e19789e
逗叔 <yfb1004@163.com>
2015-09-06 14:25:39 +0800

    项目初始化

---
+++ weiboInSwift/weiboInSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  weiboInSwift
+//
+//  Created by 逗叔 on 15/9/6.
+//  Copyright © 2015年 逗叔. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
